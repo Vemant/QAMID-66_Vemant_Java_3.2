536f6666c46f90dbc39895c316f6ce76f0566d78
Mikhail Verbitskii <vemant@yandex.ru>
2023-07-04 08:42:53 +0300

    Java 3.2_2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a current account");
-        int curr_account = in.nextInt();
+        int currAccount = in.nextInt();
         System.out.println("Enter an addition");
         int addition = in.nextInt();
 
-        int fin_account = curr_account + addition;
-        System.out.println("Final account is " +  fin_account);
+        int finAccount = currAccount + addition;
+        System.out.println("Final account is " +  finAccount);
 
         if (addition > 1000) {
             System.out.println("Your bonus is " + addition / 100);
